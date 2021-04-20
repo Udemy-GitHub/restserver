9afaab596d2a67f538b989b12a0ec186b6b876f7
Udemy-Github <udemygit@gmail.com>
2021-04-20 18:55:54 -0500

    Google Sign In

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/user')
 const bcryptjs = require('bcryptjs')
 const { generatejwt } = require('../helpers/generate-jwt')
+const { googleVerify } = require('../helpers/google-verify')
 
 const login = async (req, res) => {
  
@@ -41,6 +42,55 @@ const login = async (req, res) => {
     
 }
 
+const googleSignIn = async (req, res) => {
+
+    const { id_token } = req.body
+
+    try {
+
+        const { name, email, img } = await googleVerify(id_token)
+
+        let user = await User.findOne({ email })
+
+        if (!user) {
+
+            const data = {
+                name,
+                email,
+                password: ':)',
+                img,
+                google: true
+            }
+
+            user = new User(data)
+            await user.save()
+
+        }
+
+        if (!user.status) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        const token = await generatejwt( user.id )
+
+        res.json({
+            user,
+            token
+        })
+
+    } catch (error) {
+
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
+        })
+
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
